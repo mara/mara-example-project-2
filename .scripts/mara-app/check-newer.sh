3d7a7d6bf2fc9545d3abb5a90d7787b9ed075ede
Martin Loetzsch <martin.loetzsch@gmail.com>
2020-02-13 10:25:29 +0100

    Get working on Python 3.8 and Catalina (by updating some packages)

--- .scripts/mara-app/check-newer.sh
+++ .scripts/mara-app/check-newer.sh
@@ -10,9 +10,10 @@ git fetch --all --quiet 2>&1
 
 if [[ $(git tag) ]]; then
    current_tag=`git describe --tags`
-   origin_head_tag=`git describe origin/master --tags`
-   if [ $current_tag != $origin_head_tag ]; then
-       echo -e "\033[32m newer version available for $1 ($current_tag -> $origin_head_tag)\033[0m"
+   latest_tagged_commit=`git rev-list --tags --max-count=1`
+   latest_tag=`git describe --tags $latest_tagged_commit`
+   if [ $current_tag != $latest_tag ]; then
+       echo -e "\033[32m newer version available for $1 ($current_tag -> $latest_tag)\033[0m"
    fi
 fi
 
