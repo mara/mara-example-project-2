08bac1e07dd0ddf1ca4d8780db42394a565d62fd
Martin Loetzsch <martin.loetzsch@gmail.com>
2020-03-05 10:59:14 +0100

    wip

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 
 import pathlib
 import sys
+from shutil import copy
 
 # configure application and packages
 import app.data_integration
@@ -11,8 +12,8 @@ import app.ui
 
 # apply environment specific settings (not in git repo)
 if not pathlib.Path(__file__).parent.joinpath('local_setup.py').exists():
-    sys.stderr.write('Please copy app/local_setup.py.example to app/local_setup.py and adapt\n')
-    sys.exit(-1)
-else:
-    import app.local_setup
+    sys.stderr.write('IMPORTANT: Local configuration was adapted automatically from app/local_setup.py.example '
+                     'to app/local_setup.py. Please check configuration and adapt any changes accordingly.\n')
+    copy('app/local_setup.py.example', 'app/local_setup.py')
 
+import app.local_setup
